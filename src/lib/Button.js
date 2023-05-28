2fa314b8a6ec9075e7c9d372988cb74e633b05c4
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-05-28 14:27:23 +0530

    v1 Delta 1.16 || Removed one error

--- src/lib/Button.js
+++ src/lib/Button.js
@@ -43,11 +43,7 @@ Button.propTypes = {
             + Number(!!success)
             + Number(!!warning)
             + Number(!!danger);
-        if (sumnum === 1) {
-            return;
-        } else if (sumnum === 0) {
-            return new Error("Please provide the type of button as a prop on the Button jsx element");
-        } else {
+        if (sumnum > 1) {
             return new Error("Please provide only one button type as a prop on the Button jsx element");
         };
     }
