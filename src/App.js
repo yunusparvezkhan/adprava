3dd13aa78f13e7269fa36ec8b531e05480871171
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-05-31 00:30:31 +0530

    v1 Delta 2.3 || Some styles

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Accordion from "./components/Accordion";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-4xl text-center">Adprava</h1>
+      <h1 className="text-5xl font-semibold text-gray-100 py-5 bg-gray-800 pl-20">Adprava</h1>
       {/* <ButtonPage /> */}
       <Accordion />
     </div >
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { HiQuestionMarkCircle } from 'react-icons/hi';
+import { MdQuestionAnswer } from 'react-icons/md';
 
 const Accordion = () => {
 
@@ -29,9 +31,15 @@ const Accordion = () => {
     const renderData = () => {
         return qnadata.map(({ q, a }) => {
             return (
-                <div>
-                    <h3>{q}</h3>
-                    <p>{a}</p>
+                <div className='ml-20 mt-7'>
+                    <div className='flex items-center'>
+                        <HiQuestionMarkCircle className='mr-2' />
+                        <h3 className='text-xl font-medium'>{q}</h3>
+                    </div>
+                    <div className='flex items-center'>
+                        <MdQuestionAnswer className='mr-2' />
+                        <p className='text'>{a}</p>
+                    </div>
                 </div>
             )
         })
@@ -39,7 +47,7 @@ const Accordion = () => {
 
     return (
         <div>
-            <h1 className='text-2xl text-center mt-5' >Accordion</h1>
+            <h1 className='text-3xl mt-5 ml-20 font-black text-violet-700' >Accordion</h1>
             <div>
                 {renderData()}
             </div>
