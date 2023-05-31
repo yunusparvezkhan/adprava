44b0a73e550ef8b48a04a539295f18bb6708e8f0
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-05-31 20:42:12 +0530

    v1 Delta 2.4 || Created toggling arrow button
    Now each accordion item has a arrow after it, that toggles direction on click. This yet to be used in future to togle the answers

--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import { MdQuestionAnswer } from 'react-icons/md';
+import React, { useState } from 'react'
+import { MdQuestionAnswer, MdExpandMore, MdExpandLess } from 'react-icons/md';
 import { HiQuestionMarkCircle } from 'react-icons/hi';
 
 const AccordionItem = ({ q, a }) => {
+    const [AccordionSts, setAccordionSts] = useState(false);
+    const [DownIcon, setDownIcon] = useState('');
+    const [UpIcon, setUpIcon] = useState('hidden');
+    const handleExpandIconClick = () => {
+        if (AccordionSts === false) {
+            setDownIcon('hidden');
+            setUpIcon('');
+            setAccordionSts(true);
+        } else if (AccordionSts === true) {
+            setDownIcon('');
+            setUpIcon('hidden')
+            setAccordionSts(false);
+        }
+    }
     return (
         <div className='ml-20 mt-7'>
             <div className='flex items-center'>
                 <HiQuestionMarkCircle className='mr-2' />
                 <h3 className='text-xl font-medium'>{q}</h3>
+                <button onClick={handleExpandIconClick} >
+                    <MdExpandMore className={'ml-2 text-2xl ' + DownIcon} />
+                    <MdExpandLess className={'ml-2 text-2xl ' + UpIcon} />
+                </button>
             </div>
             <div className='flex items-center'>
                 <MdQuestionAnswer className='mr-2' />
