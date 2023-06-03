41b2d0999a9b399ba9d144726b54f2d22edf7c43
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-03 15:46:49 +0530

    v1 Delta 3.3 || Some styles

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,16 +4,17 @@ const Dropdown = ({ DropDownOptions }) => {
     const [selectedOption, setSelectedOption] = useState(null);
     const renderMenu = DropDownOptions.map((option) => {
         return (
-            <div onClick={() => setSelectedOption(option)} >
+            <div onClick={() => setSelectedOption(option)} className='py-2 px-2 block' >
                 <label className='text-base cursor-pointer' >{option.q}</label>
             </div>
         )
     })
     return (
-        <div className='text-2xl' >
+        <div className='ml-20 mt-10 w-1/3 bg-gray-100 ' >
             <div>
                 <div className='text-3xl font-bold' >
                     {selectedOption ? selectedOption.q : 'Select'}
+
                 </div>
                 {renderMenu}
             </div>
