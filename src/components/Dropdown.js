c906b180a903ba3e86c0b49748be52c223bab360
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-03 15:51:15 +0530

    v1 Delta 3.4 || Created the toggling feature of the Dropdown menu

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 
 const Dropdown = ({ DropDownOptions }) => {
     const [selectedOption, setSelectedOption] = useState(null);
+    const [menuhider, setMenuhider] = useState(true);
     const renderMenu = DropDownOptions.map((option) => {
         return (
-            <div onClick={() => setSelectedOption(option)} className='py-2 px-2 block' >
+            <div onClick={() => { setSelectedOption(option); setMenuhider(true) }} className='py-2 px-2 block' >
                 <label className='text-base cursor-pointer' >{option.q}</label>
             </div>
         )
@@ -12,11 +13,12 @@ const Dropdown = ({ DropDownOptions }) => {
     return (
         <div className='ml-20 mt-10 w-1/3 bg-gray-100 ' >
             <div>
-                <div className='text-3xl font-bold' >
+                <div className='text-3xl font-bold cursor-pointer' onClick={() => setMenuhider(!menuhider)} >
                     {selectedOption ? selectedOption.q : 'Select'}
-
                 </div>
-                {renderMenu}
+                <div className={menuhider === false ? '' : 'hidden'}>
+                    {renderMenu}
+                </div>
             </div>
         </div>
     )
