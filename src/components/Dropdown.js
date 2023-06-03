3b1338c45849137156fb12ed803d3ae60310be64
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-03 15:37:20 +0530

    v1 Delta 3.2 || Printed out all the options and made the selection mechanism in Dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Dropdown = () => {
+const Dropdown = ({ DropDownOptions }) => {
+    const [selectedOption, setSelectedOption] = useState(null);
+    const renderMenu = DropDownOptions.map((option) => {
+        return (
+            <div onClick={() => setSelectedOption(option)} >
+                <label className='text-base cursor-pointer' >{option.q}</label>
+            </div>
+        )
+    })
     return (
-        <div className='text-2xl' >Dropdown</div>
+        <div className='text-2xl' >
+            <div>
+                <div className='text-3xl font-bold' >
+                    {selectedOption ? selectedOption.q : 'Select'}
+                </div>
+                {renderMenu}
+            </div>
+        </div>
     )
 }
 
