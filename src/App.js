7b8a4f999cbf737c1a829b3fbf8468268cec8106
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-03 13:43:40 +0530

    v1 Delta 3.0 || Initialized a new component 'Dropdown'
    The 'Dropdown' component will be simply a dropdown menu with some data being fetched from any dataset, rather than hard coding.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ButtonPage from "./pages/ButtonPage";
 import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 
 function App() {
   const qnadata = [
@@ -35,7 +36,8 @@ function App() {
     <div className="App">
       <h1 className="text-5xl font-semibold text-gray-100 py-5 bg-gray-800 pl-20">Adprava</h1>
       {/* <ButtonPage /> */}
-      <Accordion qnadata={qnadata} />
+      {/* <Accordion qnadata={qnadata} /> */}
+      <Dropdown />
     </div >
   );
 }
