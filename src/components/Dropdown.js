1c364ca3feb288204db725d6e7f265a6da96cd12
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-03 16:15:50 +0530

    v1 Delta 3.5 || Completed icon logics in Dropdown toggling

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
+import { MdExpandMore, MdExpandLess, MdOutlineCheckBoxOutlineBlank, MdOutlineCheckBox } from 'react-icons/md';
+import { HiQuestionMarkCircle } from 'react-icons/hi';
 
 const Dropdown = ({ DropDownOptions }) => {
     const [selectedOption, setSelectedOption] = useState(null);
     const [menuhider, setMenuhider] = useState(true);
     const renderMenu = DropDownOptions.map((option) => {
         return (
-            <div onClick={() => { setSelectedOption(option); setMenuhider(true) }} className='py-2 px-2 block' >
+            <div onClick={() => { setSelectedOption(option); setMenuhider(true) }} className='py-2 block flex items-center' >
+                <MdOutlineCheckBoxOutlineBlank className={selectedOption === option ? 'hidden' : 'mr-2'} />
+                <MdOutlineCheckBox className={selectedOption === option ? 'mr-2' : 'hidden'} />
                 <label className='text-base cursor-pointer' >{option.q}</label>
             </div>
         )
@@ -13,8 +17,12 @@ const Dropdown = ({ DropDownOptions }) => {
     return (
         <div className='ml-20 mt-10 w-1/3 bg-gray-100 ' >
             <div>
-                <div className='text-3xl font-bold cursor-pointer' onClick={() => setMenuhider(!menuhider)} >
-                    {selectedOption ? selectedOption.q : 'Select'}
+                <div className='text-3xl font-bold cursor-pointer flex items-center' onClick={() => setMenuhider(!menuhider)} >
+                    <div className='flex-1 py-2'>
+                        {selectedOption ? selectedOption.q : 'Select'}
+                    </div>
+                    <MdExpandMore className={menuhider === true ? 'flex-2' : 'hidden'} />
+                    <MdExpandLess className={menuhider === false ? 'flex-2' : 'hidden'} />
                 </div>
                 <div className={menuhider === false ? '' : 'hidden'}>
                     {renderMenu}
