c9f6d0f4129a9d55904df8d7b070b0d515aef444
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-04 22:36:21 +0530

    v1 Delta cleanup 2.9.1 || Removed 'qnadata' from App.js

--- src/App.js
+++ src/App.js
@@ -6,34 +6,6 @@ import DropdownPage from "./pages/DropdownPage";
 
 function App() {
   const [bodyclick, setBodyclick] = useState(false);
-  const qnadata = [
-    {
-      id: 1,
-      q: "Who invented Gravity?",
-      a: "Ancient Indian Rishi, Brahmagupta II invented gravity."
-    },
-    {
-      id: 2,
-      q: "Who invented Surgery?",
-      a: "Ancient Indian Medical practisioner Sushruta Samhita came up with the theory of surgery."
-    },
-    {
-      id: 3,
-      q: "Who invented Electromagnetic Waves?",
-      a: "Profound British Indian Bengali Scientist Acharya jagadish Chandra Bose invented Electromagnetic Waves."
-    },
-    {
-      id: 4,
-      q: "Who discovered the value of pi?",
-      a: "Rishi Aryabhatta calculated the value of pi to be 62832/20000 = 3.1416"
-    },
-    {
-      id: 5,
-      q: "Who discovered the shape of the earth?",
-      a: "Aryabhatta was among the first people who gave the oval shape theory of earth."
-    },
-
-  ]
 
   return (
     <div className="App">
