cc99d81e25d2e13961253d854c9476fb090c7fbf
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-04 23:19:33 +0530

    v1 Delta cleanup 3.11.1

--- src/pages/AccordionPage.js
+++ src/pages/AccordionPage.js
@@ -5,7 +5,6 @@ import { qnadata } from '../data/qnadata'
 const AccordionPage = () => {
     return (
         <div>
-            {/* This change got pushed on previous commit `APOLOGY n1`, hence making new commit explaining it */}
             <h1 className='text-3xl mt-5 ml-20 font-black text-violet-700' >Accordion</h1>
             <Accordion qnadata={qnadata} />
         </div>
