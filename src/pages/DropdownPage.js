1107950c80e3aa285baa8dc8f7f97f9475622b3d
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-04 22:47:45 +0530

    v1 Delta 2.10 || Provided a header on 'DropdownPage'

--- src/pages/DropdownPage.js
+++ src/pages/DropdownPage.js
@@ -6,6 +6,7 @@ import { qnadata } from '../data/qnadata';
 const DropdownPage = () => {
     return (
         <div>
+            <h2 className='text-3xl mt-5 ml-20 font-black text-violet-700' >Dropdown Page</h2>
             <Dropdown DropDownOptions={qnadata} />
         </div>
     )
