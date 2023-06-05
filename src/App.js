f5e2fa176c048985ded9f260981130691f2fc75b
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-05 17:32:27 +0530

    v1 Delta 3.13 || Basic Routing
    Made basic routing logic using if else statements

--- src/App.js
+++ src/App.js
@@ -6,12 +6,27 @@ import NavBar from "./components/NavBar";
 
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('./Accordion')
+  const onCurrentPageChange = (value) => {
+    setCurrentPage(value);
+  }
+
+  const renderPage = () => {
+    if (window.location.pathname === '/Button') {
+      return <div><ButtonPage /></div>
+    } else if (window.location.pathname === '/Accordion' || window.location.pathname === '/') {
+      return <AccordionPage />
+    } else if (window.location.pathname === '/Dropdown') {
+      return <DropdownPage />
+    }
+  }
+
   return (
     <div className="App">
-      <NavBar />
-      {/* <ButtonPage /> */}
-      {/* <AccordionPage /> */}
-      <DropdownPage />
+      <NavBar onChange={onCurrentPageChange} />
+      <div>
+        {renderPage()}
+      </div>
     </div >
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,16 +4,20 @@ import Button from './Button';
 import '../styles/stylelib.css';
 
 
-const NavBar = () => {
+const NavBar = ({ onChange }) => {
+    const onBtnClick = (path) => {
+        window.history.pushState({}, '', path);
+        onChange(path);
+    }
     return (
         <div>
             <div className="bg-gray-800 flex items-center">
                 <img src={adpravaLogo} className='pl-20' />
                 <h1 className='text-5xl font-semibold text-gray-100 py-5 pl-2' >Adprava</h1>
                 <div className='navbar-button-cont flex ' >
-                    <Button secondary >Accordion</Button>
-                    <Button secondary >Button</Button>
-                    <Button secondary >Dropdown</Button>
+                    <Button secondary onClick={() => onBtnClick('/Accordion')} >Accordion</Button>
+                    <Button secondary onClick={() => onBtnClick('/Button')}>Button</Button>
+                    <Button secondary onClick={() => onBtnClick('/Dropdown')}>Dropdown</Button>
                 </div>
             </div>
         </div>
