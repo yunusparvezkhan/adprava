74306a9a7e2c8546790fe07acb3de4ea1d977d03
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-07 16:44:57 +0530

    v1 Delta  fix 3.29.2 || fixed function invoke in Link.js

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import useNavigation from '../hooks/navigationHook';
 
-const Link = ({ to, children }) => {
-    const { navigate } = useNavigation;
+const Link = ({ to, children, ...rest }) => {
+    const { navigate } = useNavigation();
 
     const handleClick = (event) => {
         if (event.ctrlKey || event.metaKey) {
@@ -14,7 +14,7 @@ const Link = ({ to, children }) => {
 
     return (
         <div className='mx-1 px-3 py-2 hover:bg-blue-900 transition duraiton-100 cursor-pointer ' >
-            <a href={to} onClick={handleClick} >{children}</a>
+            <a {...rest} href={to} onClick={handleClick} >{children}</a>
         </div>
     )
 }
