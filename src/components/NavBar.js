beb68939edb026c50bc268353c047ebe2cdce5cd
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-07 17:14:01 +0530

    v1 Delta  3.32 || managed repeated code > The Link JSX element in NavBar.js was being passed with repeated className logics, and repeated path strings. To tackle that issue, created a array of objects in NavBar.js named [links], then mapped that array with returning a Link jsx element with the className logics and fetching the strings dirently from the objects.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,25 @@ import '../styles/stylelib.css';
 const NavBar = () => {
     const { currentPath } = useNavigation();
 
+    const links = [
+        {
+            label: 'Accordion',
+            path: '/Accordion',
+            secondaryPath: '/'
+        },
+        {
+            label: 'Dropdown',
+            path: '/Dropdown',
+            secondaryPath: ''
+        },
+        {
+            label: 'Button',
+            path: '/Button',
+            secondaryPath: ''
+        },
+
+    ];
+
     const stylesfunc = (path) => {
         if (currentPath === path) {
             return 'text-green-400';
@@ -15,15 +34,36 @@ const NavBar = () => {
         }
     }
 
+    const renderLinks = links.map((link) => {
+        return (
+            <div key={link.path}>
+                <Link className={stylesfunc(link.path) + stylesfunc(link.secondaryPath)} to={link.path}>{link.label}</Link>
+            </div>
+        )
+    })
+
     return (
         <div>
             <div className="bg-gray-800 flex items-center">
                 <img src={adpravaLogo} className='pl-20' />
                 <h1 className='text-5xl font-semibold text-gray-100 py-5 pl-2' >Adprava</h1>
                 <div className='navbar-button-cont flex text-white ' >
-                    <Link className={stylesfunc('/Accordion') + stylesfunc('/')} to='/Accordion' >Accordion</Link>
-                    <Link className={stylesfunc('/Dropdown')} to='/Dropdown' >Dropdown</Link>
-                    <Link className={stylesfunc('/Button')} to='/Button' >Buttons</Link>
+
+                    {/* This Link render logic was used up until this point. But now we have created new array of objects called links in line 10. And then
+                            mapping that in line 37, where we used the Link component. By doing this repeated code is avoided.                                          */}
+                    {/* -------------------------------------------------------------------------------------------------------------------------------------// 
+                    <Link className={stylesfunc('/Accordion') + stylesfunc('/')} to='/Accordion' >Accordion</Link>                                          //      
+                    <Link className={stylesfunc('/Dropdown')} to='/Dropdown' >Dropdown</Link>                                                              //
+                    <Link className={stylesfunc('/Button')} to='/Button' >Buttons</Link>                                                                  //            */}
+                    {/* ---------------------------------------------------------------------------------------------------------------------------------//             */}
+
+
+                    {/* The value from the map function is being rendered here */}
+                    {/* ----------------------------------------------------------- //     */}
+                    {renderLinks}                                           {/*    //      */}            {/*   <---   NEW LOGIC HERE       */}
+                    {/* ----------------------------------------------------------//       */}
+
+
                 </div>
             </div>
         </div >
