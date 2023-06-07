b0a2fd44eca031b12a21b913d05c06dbc12b564b
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-07 17:15:12 +0530

    v1 Delta  cleanup 3.32.1 || Cleaned up comments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -48,22 +48,7 @@ const NavBar = () => {
                 <img src={adpravaLogo} className='pl-20' />
                 <h1 className='text-5xl font-semibold text-gray-100 py-5 pl-2' >Adprava</h1>
                 <div className='navbar-button-cont flex text-white ' >
-
-                    {/* This Link render logic was used up until this point. But now we have created new array of objects called links in line 10. And then
-                            mapping that in line 37, where we used the Link component. By doing this repeated code is avoided.                                          */}
-                    {/* -------------------------------------------------------------------------------------------------------------------------------------// 
-                    <Link className={stylesfunc('/Accordion') + stylesfunc('/')} to='/Accordion' >Accordion</Link>                                          //      
-                    <Link className={stylesfunc('/Dropdown')} to='/Dropdown' >Dropdown</Link>                                                              //
-                    <Link className={stylesfunc('/Button')} to='/Button' >Buttons</Link>                                                                  //            */}
-                    {/* ---------------------------------------------------------------------------------------------------------------------------------//             */}
-
-
-                    {/* The value from the map function is being rendered here */}
-                    {/* ----------------------------------------------------------- //     */}
-                    {renderLinks}                                           {/*    //      */}            {/*   <---   NEW LOGIC HERE       */}
-                    {/* ----------------------------------------------------------//       */}
-
-
+                    {renderLinks}
                 </div>
             </div>
         </div >
