4899752d9dd531af4bb00f93818ff2a4102c81e7
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-07 13:53:41 +0530

    v1 Delta 3.23 || Link button styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
             <div className="bg-gray-800 flex items-center">
                 <img src={adpravaLogo} className='pl-20' />
                 <h1 className='text-5xl font-semibold text-gray-100 py-5 pl-2' >Adprava</h1>
-                <div className='navbar-button-cont flex ' >
+                <div className='navbar-button-cont flex text-white ' >
                     <Link to='/Accordion' >Accordion</Link>
                     <Link to='/Dropdown' >Dropdown</Link>
                     <Link to='/Button' >Buttons</Link>
