dcdc75e4bb4aa6fa4cdc9f3bc03de56082cb0ffe
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-07 16:46:25 +0530

    v1 Delta  3.30 || NavBar styles
    Created NavBar active page font-color. That means that whichever page is open in the url bar, Link to that page will be in a different color

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import adpravaLogo from '../media/icons/gold-medal.png';
 import Link from './Link';
+import useNavigation from '../hooks/navigationHook';
 import '../styles/stylelib.css';
 
 const NavBar = () => {
+    const { currentPath } = useNavigation();
+
+    const stylesfunc = (path) => {
+        if (currentPath === path) {
+            return 'text-green-400';
+        } else {
+            return '';
+        }
+    }
+
     return (
         <div>
             <div className="bg-gray-800 flex items-center">
                 <img src={adpravaLogo} className='pl-20' />
                 <h1 className='text-5xl font-semibold text-gray-100 py-5 pl-2' >Adprava</h1>
                 <div className='navbar-button-cont flex text-white ' >
-                    <Link to='/Accordion' >Accordion</Link>
-                    <Link to='/Dropdown' >Dropdown</Link>
-                    <Link to='/Button' >Buttons</Link>
+                    <Link className={stylesfunc('/Accordion')} to='/Accordion' >Accordion</Link>
+                    <Link className={stylesfunc('/Dropdown')} to='/Dropdown' >Dropdown</Link>
+                    <Link className={stylesfunc('/Button')} to='/Button' >Buttons</Link>
                 </div>
             </div>
         </div >
