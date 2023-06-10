b89c60ed96ce762e1745a2758d3a92edc5857eaf
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-10 17:37:06 +0530

    v2 Delta 1.6 || Added Modal Buttons
    Added two Buttons in Modal component. One for conforming the modal, and another for cancelling. In the upcoming codes,
    the logics for sending message to parent component will be done.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './styles/modal.css';
+import Button from './Button';
 
 const Modal = ({ closeModal }) => {
     const [boxClickHandler, setBoxClickHandler] = useState(false);
@@ -18,7 +19,21 @@ const Modal = ({ closeModal }) => {
         <div>
             <div className='modal-background' onClick={() => setBgClickHandler(true)} >
                 <div className='modal-box' onClick={() => setBoxClickHandler(true)} >
-
+                    <div className='flex items-center'>
+                        <p className='block p-20 text-xl '>
+                            Hey, here is some message for you. This is a modal box a a a a a and you have to accept the conditions.
+                        </p>
+                    </div>
+                    <div className='modal-btns'>
+                        <div className='flex'>
+                            <div className='modal-neg-btn' >
+                                <Button danger >Cancel</Button>
+                            </div>
+                            <div className='modal-aff-btn' >
+                                <Button success >Proceed</Button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div >
