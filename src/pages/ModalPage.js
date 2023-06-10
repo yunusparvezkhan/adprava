395e815127be95d8589c3a95896ab36459c25439
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-10 16:38:58 +0530

    v2 Delta 1.4 || Created [Modal] togling feature
    Created logics in Modal Page to toggle the Modal component by clicking the Button

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Modal from '../components/Modal'
 import Button from '../components/Button.js'
 
 const ModalPage = () => {
+    const [modalsts, setmodalsts] = useState(false);
+
+    const handleBtnClick = () => {
+        setmodalsts(!modalsts)
+    }
+
+    const rendermodal = () => {
+        if (modalsts) {
+            return <Modal />
+        } else {
+            return;
+        }
+    }
     return (
         <div>
             <h2 className='text-3xl mt-5 ml-20 font-black text-violet-700' >Modal Page</h2>
             <div className='ml-20 mt-10' >
-                <Button primary rounded>Show Modal</Button>
+                <Button primary rounded onClick={handleBtnClick} >Show Modal</Button>
             </div>
-            <Modal />
+            <div>
+                {rendermodal()}
+            </div>
         </div>
     )
 }
