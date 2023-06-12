dbb2ff5cf2ca3b2b63f1ca8181a4cd7dab61df36
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-12 14:28:43 +0530

    v3 Delta 1.3 || Rendered a basic table
    Rendered a basic table by mapping the prop [data] which is supposed to be an array of objects passed by the parent
    component.

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,29 @@
 import React from 'react'
 
 const Table = ({ data }) => {
+
+    const renderRows = data.map((row) => {
+        return (
+            <tr key={row.name} >
+                <th>{row.name}</th>
+                <th className={`p-5 ${row.color}`} ></th>
+                <th>{row.count}</th>
+            </tr>
+        )
+    })
+
     return (
         <div className='ml-20 mt-10' >
             <table>
                 <thead>
                     <tr>
-                        <th>Fruit</th>
-                        <th>Color</th>
-                        <th>Count</th>
+                        <th className='px-10'>Fruit</th>
+                        <th >Color</th>
+                        <th className='px-5' >Count</th>
                     </tr>
                 </thead>
                 <tbody>
-
+                    {renderRows}
                 </tbody>
             </table>
         </div>
