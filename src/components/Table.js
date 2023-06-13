15d498cb98ffc2e52df333d14404ced8c7c35027
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-13 23:03:54 +0530

    v3 Delta 1.4 || Table styles

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,19 +4,21 @@ const Table = ({ data }) => {
 
     const renderRows = data.map((row) => {
         return (
-            <tr key={row.name} >
-                <th>{row.name}</th>
-                <th className={`p-5 ${row.color}`} ></th>
-                <th>{row.count}</th>
+            <tr key={row.name} className='border-b'  >
+                <td className='p-5' >{row.name}</td>
+                <td className='p-5' >
+                    <div className={`p-5 m-2 ${row.color}`} ></div>
+                </td>
+                <td className='p-5'>{row.count}</td>
             </tr>
         )
     })
 
     return (
         <div className='ml-20 mt-10' >
-            <table>
+            <table className='table-auto border-spacing-2' >
                 <thead>
-                    <tr>
+                    <tr className='border-b-2' >
                         <th className='px-10'>Fruit</th>
                         <th >Color</th>
                         <th className='px-5' >Count</th>
