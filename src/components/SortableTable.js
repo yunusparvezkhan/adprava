16eb2df2e68f1c8ef47486037f52d80318992f44
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-16 22:51:51 +0530

    v3 Delta 2.3 || Created Sorting mechanism
    Created entire sorting mechanism. This commit makes another copy of the data got from parent component
    and then sorts depending upon user's input and updates the prop passing to the Table component

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Table from './Table'
 import { FaSortAlphaDown } from 'react-icons/fa'
 
 const SortableTable = (props) => {
-    const { config } = props;
+    const [sortOrder, setSortOrder] = useState(null);
+    const [sortBy, setSortBy] = useState(null);
+    const { config, data } = props;
+
 
     const handleClick = (label) => {
         console.log(label)
+        if (sortOrder === null) {
+            setSortOrder('asc');
+            setSortBy(label);
+        } else if (sortOrder === 'asc') {
+            setSortOrder('desc');
+            setSortBy(label);
+        } else {
+            setSortOrder(null)
+        }
     }
 
     const updatedConfig = config.map((col) => {
@@ -15,12 +27,34 @@ const SortableTable = (props) => {
         } else {
             return {
                 ...col,
-                header: () => <th onClick={() => handleClick(col.label)} className='cursor-pointer' > <div className='flex items-center mx-2'> <FaSortAlphaDown className='mx-1' />{col.label}</div></th>
+                header: () => <th onClick={() => handleClick(col.label)} className='cursor-pointer'> <div className='flex items-center mx-2'> <FaSortAlphaDown className='mx-1' />{col.label}</div></th>
             }
         }
     })
+
+
+    let sortedData = data;
+    if (sortOrder && sortBy) {
+        const { sortValue } = config.find(col => col.label === sortBy);
+        sortedData = [...data].sort((a, b) => {
+            const valueA = sortValue(a);
+            const valueB = sortValue(b);
+
+            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+
+            if (typeof valueA === 'string') {
+                return valueA.localeCompare(valueB) * reverseOrder;
+            } else if (typeof valueA === 'number') {
+                return (valueA - valueB) * reverseOrder;
+            };
+        });
+    };
+
     return (
-        <Table {...props} config={updatedConfig} />
+        <div>
+            {sortOrder} --- {sortBy}
+            <Table {...props} data={sortedData} config={updatedConfig} />
+        </div>
     )
 }
 
