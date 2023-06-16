98b50372ae1b74fb94a943049c4a0c444f816de0
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-16 15:45:40 +0530

    v3 Delta 2.0 || Created SortableTable component
    Created a new component [SortableTable] for sorted tables

---
+++ src/components/SortableTable.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Table from './Table'
+
+const SortableTable = (props) => {
+    return (
+        <Table {...props} />
+    )
+}
+
+export default SortableTable
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Table from '../components/Table'
-import { string } from 'prop-types';
+import SortableTable from '../components/SortableTable'
+
 
 const TablePage = () => {
     const data = [
@@ -42,7 +43,7 @@ const TablePage = () => {
     return (
         <div>
             <h2 className='text-3xl mt-5 ml-20 font-black text-violet-700' >Table Page</h2>
-            <Table data={data} config={config} keyFn={keyFn} />
+            <SortableTable data={data} config={config} keyFn={keyFn} />
         </div>
     )
 }
