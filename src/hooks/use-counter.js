23d9fd86c7da79087cf231c3f33431b8f70c5ff5
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-20 00:41:55 +0530

    v4 Delta 1.3 || Used Immer lib in useCounter custom hook
    Used the Immer lib in our custom hook useCounter to reduce code.
    This library allows to directly assing values to reducer states.
    After assignation of values to a state, immer takes those values and
    creates a obejct with all the other states and passes that updated
    state. This object then gets passed with the dispatched function.

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,25 +1,48 @@
+import { produce } from 'immer';
 import { useReducer } from 'react';
 
-
 const reducer = (state, action) => {
-    switch (action.type) {
-        case 'CHANGE_COUNT':
-            return {
-                ...state,
-                count: state.count + action.payload,
-            }
-        case 'CHANGE_INCREMENTBY':
-            return {
-                ...state,
-                incrementBy: action.payload
-            }
-        default:
-            return state;
-    }
-}
+    // This is how useReducer states should be updated. Which was followed previously. 
+    // But now we have an optional alternative
+    // ----------------------------------------------------------------------//
+    // ---------------------------------------------------------------------//
+    // switch (action.type) {                                              //
+    //     case 'CHANGE_COUNT':                                           //
+    //         return {                                                  //
+    //             ...state,                                            //
+    //             count: state.count + action.payload,                //
+    //         }                                                      //
+    //     case 'CHANGE_INCREMENTBY':                                //
+    //         return {                                             //
+    //             ...state,                                       //
+    //             incrementBy: action.payload                    //
+    //         }                                                 //
+    //     default:                                             //
+    //         return state;                                   //
+    // }                                                      //
+    // ------------------------------------------------------//
+
+
+    // Assinging to the state values directly like this can be achieved through immer library,
+    // which is imported at the top and the reducer function is wrapped inside the produce function
+    // from immer in the first parameter of useReducer declaration in line 45.
+    // --------------------------------------------------------------//
+    // -------------------------------------------------------------//
+    switch (action.type) {                                         //
+        case 'CHANGE_COUNT':                                      //
+            state.count = state.count + action.payload;          //
+            return;                                             //
+        case 'CHANGE_INCREMENTBY':                             //
+            state.incrementBy = action.payload;               //
+            return;                                          //
+        default:                                            //
+            return state;                                  //
+    }                                                     //
+}                                                        //
+// -----------------------------------------------------//
 
 const useCounter = (initialCount) => {
-    const [state, dispatch] = useReducer(reducer, {
+    const [state, dispatch] = useReducer(produce(reducer), {
         count: initialCount,
         incrementBy: 5
     })
