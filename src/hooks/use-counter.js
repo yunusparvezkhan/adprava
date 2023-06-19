c6cffb833f839cff24347d4ba68539860d8c8847
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-19 21:21:29 +0530

    v4 Delta 1.2 || Used useReducer in useCounter hook
    The useCounter hook contains two states, which are similar in nature and used in similar
    stuff. Hence restructured them using useReducer hook. And used the dispatch function to update
    the reducer states by passing a object, having one property as [type] to declare type and another
    as [payload] for the value. Then created one state setter function for [incrementBy] state and passed it
    to children components by returning it in the returning object

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,23 +1,62 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
+
+
+const reducer = (state, action) => {
+    if (action.type === 'CHANGE_COUNT') {
+        return {
+            ...state,
+            count: state.count + action.payload
+        }
+    } else if (action.type === 'CHANGE_INCREMENTBY') {
+        return {
+            ...state,
+            incrementBy: action.payload
+        }
+    }
+}
 
 const useCounter = (initialCount) => {
-    const [count, setCount] = useState(initialCount);
-    const [incrementBy, setIncrementBy] = useState(5);
+    // const [count, setCount] = useState(initialCount);
+    // const [incrementBy, setIncrementBy] = useState(5);
+
+    const [state, dispatch] = useReducer(reducer, {
+        count: initialCount,
+        incrementBy: 5
+    })
 
     const handleIncrement = () => {
-        setCount(count + 1);
+        // setCount(count + 1);
+        dispatch({
+            type: 'CHANGE_COUNT',
+            payload: 1,
+        })
     }
 
     const handleDecrement = () => {
-        setCount(count - 1);
+        // setCount(count - 1);
+        dispatch({
+            type: 'CHANGE_COUNT',
+            payload: -1,
+        })
     }
 
     const handleIncrementByAmount = () => {
-        setCount(count + Number(incrementBy))
+        // setCount(count + Number(incrementBy))
+        dispatch({
+            type: 'CHANGE_COUNT',
+            payload: state.incrementBy,
+        })
+    }
+
+    const setIncrementBy = (value) => {
+        dispatch({
+            type: 'CHANGE_INCREMENTBY',
+            payload: Number(value),
+        })
     }
 
     return {
-        count, handleDecrement, handleIncrement, handleIncrementByAmount, incrementBy, setIncrementBy
+        count: state.count, handleDecrement, handleIncrement, handleIncrementByAmount, incrementBy: state.incrementBy, setIncrementBy
     }
 };
 
