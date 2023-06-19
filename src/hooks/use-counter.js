da3805bd7795055ec57ea888ada85059e9ae10fc
Yunus Parvez Khan <yunus12.yp@gmail.com>
2023-06-20 00:02:10 +0530

    v4 Delta migration 1.2.1 || Used Swwitch instead of ifelse
    used Switch function in reducer function of the useCounter hook, instead of ifelse conditionals.

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -2,22 +2,51 @@ import { useReducer, useState } from 'react';
 
 
 const reducer = (state, action) => {
-    if (action.type === 'CHANGE_COUNT') {
-        return {
-            ...state,
-            count: state.count + action.payload
-        }
-    } else if (action.type === 'CHANGE_INCREMENTBY') {
-        return {
-            ...state,
-            incrementBy: action.payload
-        }
+
+    // This code was used before for checking the action types and returning with correct object in the dispatch function
+    // --------------------------------------------------------------------- //
+    // if (action.type === 'CHANGE_COUNT') {                                //
+    //     return {                                                        //
+    //         ...state,                                                  //
+    //         count: state.count + action.payload                       //
+    //     }                                                            //
+    // } else if (action.type === 'CHANGE_INCREMENTBY') {              //                      <--- Previous Code Here (Before [v4 Delta 1.2.1])
+    //     return {                                                   //
+    //         ...state,                                             //
+    //         incrementBy: action.payload                          //
+    //     }                                                       //
+    // }                                                          //
+    // ----------------------------------------------------------//
+
+
+    switch (action.type) {
+        case 'CHANGE_COUNT':
+            return {
+                ...state,
+                count: state.count + action.payload,
+            }
+        case 'CHANGE_INCREMENTBY':
+            return {
+                ...state,
+                incrementBy: action.payload
+            }
+        default:
+            return state;
     }
+
 }
 
 const useCounter = (initialCount) => {
-    // const [count, setCount] = useState(initialCount);
-    // const [incrementBy, setIncrementBy] = useState(5);
+
+    // This useState declaration was used before using the useReducer hook for state manegement in commit [v4 Delta 1.2]
+    // And there are setter function of useState hook below in the handler function, which are commented out.
+    // ------------------------------------------------------------------------------//
+    // -----------------------------------------------------------------------------//
+    // const [count, setCount] = useState(initialCount);                           //           <--- Previous Code here (Before use of useReducer)
+    // const [incrementBy, setIncrementBy] = useState(5);                         //
+    // --------------------------------------------------------------------------//
+
+
 
     const [state, dispatch] = useReducer(reducer, {
         count: initialCount,
